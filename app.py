879a6436e5909d630f5a1052704e98902b009894
pinabel <francois.pinabel@pm.me>
2025-02-25 04:08:03 +0100

    Ajout de la route /raoul

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, Raoul is alive!'
+ return 'Hello, Raoul is alive!'
+
+@app.route('/raoul')
+def raoul():
+ return 'Raoul balance sa punchline : "T’as cru que j’étais un bot lambda ?"'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+ app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
